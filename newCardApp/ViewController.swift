c77640e44706d2b2f78f40f563d9e0d43c652456
bestriess <balnur00@mail.ru>
2020-02-07 15:44:20 +0600

    Initial Commit

---
+++ newCardApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newCardApp
+//
+//  Created by Balnur Sakhybekova on 2/7/20.
+//  Copyright © 2020 Balnur Sakhybekova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
